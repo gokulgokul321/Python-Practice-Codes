bb8a6d4560fed0fbd0612eaeb2941cd8f503c3e9
gokulgokul321 <gokulgokul10005@gmail.com>
2025-01-08 16:42:39 -0800

    First message

---
+++ Day1/Basics.py
@@ -0,0 +1,16 @@
+#Scripiting way for writing the code:
+print("Hello world")
+
+#Procedural Way of writing the code
+def message():
+    print('Inside method message')
+
+message()
+
+#Object Oriented way of writing the code:
+class Demo:
+    def instance_method(self):
+        print('Inside Instance method message')
+
+d1 = Demo()
+d1.instance_method()
---
+++ VariablesDataTypes/variables.py
@@ -0,0 +1,4 @@
+#1. Python is Dynamically Typed Programming Language
+#2. In python while Declaring Variablee, we not need to mention data types.
+#3. Data Types will be assigned by language based on value 
+# stored into the variables.
